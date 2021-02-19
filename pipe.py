9c38ce02ea49ad136844efb26b316d57b2eb76ea
Mateusz Wosinski <mwosinski@yosh.ai>
2021-02-19 07:54:24 +0100

    Add Detector

--- pipe.py
+++ pipe.py
@@ -1,3 +1,4 @@
+import time
 from typing import Dict, Any
 
 import cv2
@@ -14,65 +15,75 @@ pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'  # config line
 class OCRSingleImage():
     def __init__(self,
                  lang: str = 'eng',
-                 method: str = None):
-        assert method in [None, 'simple', 'contextual'], 'Method not implemented yet!'
+                 detection_method: str = None,
+                 correction_method: str = None):
+        assert correction_method in [None, 'simple', 'contextual'], 'Method not implemented yet!'
         
         self.lang = lang
-        self.method = method
+        self.detection_method = detection_method
+        self.correction_method = correction_method
         
-        if method == 'simple':
+        if detection_method is None:
+            self.detector = lambda x: x
+        
+        if correction_method == 'simple':
             self.corrector = TypoCorrector_simple()
-        elif method == 'contextual':
+        elif correction_method == 'contextual':
             self.corrector = TypoCorrector_contextual()
-        else:
+        elif correction_method is None:
             self.corrector = lambda x: x.split(' ')
         
     def ocr_image(self,
                   img_path: str,
                   lang: str = 'eng',
-                  method = None
+                  method = None,
+                  plot: bool = False
                   ) -> str:
         
         ocr_img = cv2.imread(img_path)
         
         ocr_data = pytesseract.image_to_data(ocr_img, lang=lang, output_type=Output.DICT)
         ocr_data = self._convert_ocr_data(ocr_data)
-        ocr_text = ' '.join(ocr_data['text']).lower()
+        
+        ocr_text = self.detector(ocr_data['text'])
+        ocr_text = ' '.join(ocr_text).lower()
         
         ocr_data['text'] = self.corrector(ocr_text)
-
-        ocr_img = plot_bboxes(ocr_img, ocr_data)
+        
+        if plot:
+            ocr_img = plot_bboxes(ocr_img, ocr_data)
+        
         ocr_text = ' '.join(ocr_data['text']).lower()
         return ocr_text
     
     @staticmethod
     def _convert_ocr_data(ocr_data: Dict[str, Any]
                           ) -> Dict[str, Any]:
-        idxs = []
+        idxs_to_convert = []
         for idx, word in enumerate(ocr_data['text']):
             if word != '':
-                idxs.append(idx)
+                idxs_to_convert.append(idx)
         
-        ocr_d = {}        
-        ocr_d['left'] = np.array(ocr_data['left'])[idxs].tolist()
-        ocr_d['top'] = np.array(ocr_data['top'])[idxs].tolist()
-        ocr_d['width'] = np.array(ocr_data['width'])[idxs].tolist()
-        ocr_d['height'] = np.array(ocr_data['height'])[idxs].tolist()
-        ocr_d['text'] = np.array(ocr_data['text'])[idxs].tolist()
-        ocr_d['conf'] = np.array(ocr_data['conf'])[idxs].tolist()
+        keys_to_convert = ['left', 'top', 'width', 'height', 'text', 'conf']
+        ocr_d = {}
+        for k in keys_to_convert:
+            ocr_d[k] = np.array(ocr_data[k])[idxs_to_convert].tolist()
         return ocr_d
 
-OCR1 = OCRSingleImage(method=None)
-OCR2 = OCRSingleImage(method='simple')
-OCR3 = OCRSingleImage(method='contextual')
+OCR1 = OCRSingleImage(correction_method=None)
+OCR2 = OCRSingleImage(correction_method='simple')
+OCR3 = OCRSingleImage(correction_method='contextual')
 
+start1 = time.time()
 ocr_text1 = OCR1.ocr_image('examples/1.png', lang='eng')
-print(f'No correction: {ocr_text1}')
+print(f'\nNo correction:\n {ocr_text1}\nTime spent: {time.time() - start1}')
 
+start2 = time.time()
 ocr_text2 = OCR2.ocr_image('examples/1.png', lang='eng')
-print(f'Simple correction: {ocr_text2}')
+print(f'\nSimple correction:\n {ocr_text2}\nTime spent: {time.time() - start2}')
 
+start3 = time.time()
 ocr_text3 = OCR3.ocr_image('examples/1.png', lang='eng')
-print(f'Contextual correction: {ocr_text3}')
+print(f'\nContextual correction:\n {ocr_text3}\nTime spent: {time.time() - start3}')
 
 
