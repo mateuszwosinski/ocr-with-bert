1691abd6846f6bba20d37437e500914ee3c39ecf
Mateusz Wosinski <mwosinski@yosh.ai>
2021-03-03 15:12:10 +0100

    Minor changes

--- modules/typo_detection/typo_detection_run.py
+++ modules/typo_detection/typo_detection_run.py
@@ -4,8 +4,8 @@ from transformers import BertTokenizer
 from typo_detection_train import prepare_dataloader, train_detector
 from typo_detection_model import TypoDetectorBERT
 
-output_dir = '../data/model_init'
-data_dir = '../data/typo_ds_1'
+output_dir = '../../data/model_init'
+data_dir = '../../data/typo_ds_1'
 epochs = 5
 lr = 3e-5
 
@@ -18,7 +18,7 @@ dl_train = prepare_dataloader(inputs_path=os.path.join(data_dir, 'inputs_train.p
 dl_val= prepare_dataloader(inputs_path=os.path.join(data_dir, 'inputs_val.pt'),
                            labels_path=os.path.join(data_dir, 'labels_val.pt'),
                            masks_path=os.path.join(data_dir, 'masks_val.pt'),
-                           batch_size=64,
+                           batch_size=16,
                            mode='val')
 
 dataloaders = {'train': dl_train,
